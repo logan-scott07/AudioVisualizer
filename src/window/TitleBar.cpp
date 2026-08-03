18a83f58c88e1d6c862e1a96e12898c3ab658410
GlueMyShoe <lscott33@wisc.edu>
2026-08-03 15:19:49 -0500

    WIP TitleBar

--- src/window/TitleBar.cpp
+++ src/window/TitleBar.cpp
@@ -1,7 +1,19 @@
 #include "TitleBar.h"
+#include "Shaders.h"
 
-TitleBar::TitleBar(Window &window) : window(window) {
+TitleBar::TitleBar(Window &window, float xMin, float yMin, float xMax, float yMax)
+    : window(window),xMin(xMin), yMin(yMin), xMax(xMax), yMax(yMax) {
     hwnd = window.GetHWND();
+
+    std::vector<float> vertices {
+        xMax, yMax, 0.0f,  // top right
+        xMax, yMin, 0.0f,  // bottom right
+        xMin, yMin, 0.0f,  // bottom left
+        xMin, yMax, 0.0f,  // top left
+    };
+    std::vector<unsigned int> indices { 0, 1, 3, 1, 2, 3};
+
+    mesh = std::make_unique<Mesh>(vertices, indices);
 }
 
 void TitleBar::Initialize() {
@@ -17,4 +29,10 @@ void TitleBar::Initialize() {
         0,0,
         0,0,SWP_NOMOVE | SWP_NOSIZE |
         SWP_NOZORDER | SWP_FRAMECHANGED);
+}
+
+void TitleBar::Draw() const {
+    shader->use();
+    shader->setVec4("color", 0.15f, 0.15f, 0.15f, 1.0f);
+    mesh->Draw();
 }
--- src/window/TitleBar.h
+++ src/window/TitleBar.h
@@ -1,13 +1,22 @@
 #pragma once
 #include "Window.h"
+#include <memory>
+#include "Mesh.h"
 #include <Windows.h>
+#include "Shaders.h"
 
 class TitleBar {
-    public:
-        explicit TitleBar(Window& window);
+public:
+    explicit TitleBar(Window& window,
+        float xMin, float yMin,
+        float xMax, float yMax);
 
-        void Initialize();
-    private:
+    void Initialize();
+    void Draw() const;
+private:
     Window& window;
     HWND hwnd = nullptr;
+    float xMin, yMin, xMax, yMax;
+    std::unique_ptr<Mesh> mesh;
+    std::unique_ptr<Shaders> shader;
 };
