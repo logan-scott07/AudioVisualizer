3007b867c4d69e0870bb985888feece732e80729
Logan Scott <loganscott1225@gmail.com>
2026-08-03 19:38:04 -0500

    Implemented Changes: ShaderLink, GenQuad

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 //Standard
 #include <iostream>
 #include <memory>
+#include <optional>
 // OpenGL / Window Rendering
 #include "Shaders.h"
 #include "Window.h"
@@ -18,55 +19,38 @@
 using namespace std;
 
 int main() {
-
-    //Create window
     Window window;
 
-    if (!window.Create(1200, 500, "AudioVisualizer")) {
+    if (!window.Create(1200, 600, "AudioVisualizer")) {
+        return -1;
+    }
+    SongSelect song;
+    unique_ptr<AudioPlayer> player;
+    optional<ShaderLink> shader;
+
+    try {
+        player = make_unique<AudioPlayer>(song.Open());
+        shader.emplace(ShaderLink::Default());
+    } catch (exception &e) {
+        wcout << e.what() << endl;
         return -1;
     }
 
-    TitleBar titleBar(window, 0.0f, 0.95f, 1.0f, 1.0f);
+
+    TitleBar titleBar(window, (shader.value()),
+        Cords{ -1.0f, 0.90f, 1.0f, 1.0f },
+        Color{ .r = 0.15f, .g = 0.15f, .b = 0.2f, .a = 1.0f });
 
     titleBar.Initialize();
-
-    //Song Selection/MiniAudio player creation
-    SongSelect song;
-    unique_ptr<AUDIO_PLAYER> player;
-
-    if (song.Open()) {
-        player = make_unique<AUDIO_PLAYER>(song.GetFilePath());
-    } else {
-        wcout << L"File Selection Closed or Failed.";
-        return -1;
-    }
-
-
-    //Shader Setup
-    GLuint vertexShader;
-    GLuint fragmentShader;
-
-    string vertex_source = get_shader_source(string(SHADER_DIR) + "/default.vert");
-    string fragment_source = get_shader_source(string(SHADER_DIR) + "/default.frag");
-
-    if (!(compile_shader(vertexShader, vertex_source, GL_VERTEX_SHADER))) {
-        cout << "Vertex shader compilation failed." << endl;
-        return -1;
-    }
-
-    if (!(compile_shader(fragmentShader, fragment_source, GL_FRAGMENT_SHADER))) {
-        cout << "Fragment shader compilation failed." << endl;
-        return -1;
-    }
-
-    ShaderLink shader_link(vertexShader, fragmentShader);
 
     vector<float> vertices = generateVertices();
     vector<unsigned int> indices = generateIndices();
 
     Mesh barMesh(vertices, indices);
 
-    Button playPauseButton(0.80f, 0.80f, 0.95f, 0.95f);
+    Button playPauseButton(shader.value(),
+        Cords{ 0.80f, 0.80f, 0.95f, 0.95f },
+        Color{ .r = 1.0f, .g = 1.0f, .b = 1.0f, .a = 1.0f });
 
     bool leftMouseWasDown = false;
 
@@ -107,7 +91,7 @@ int main() {
             barMesh.UpdateVertices(vertices);
         }
 
-        shader_link.use();
+        shader->use();
         titleBar.Draw();
         barMesh.Draw();
         playPauseButton.Draw();
