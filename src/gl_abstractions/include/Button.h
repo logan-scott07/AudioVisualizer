3d91d0873f8fe10f6619e0919f5684dc7b424456
Logan Scott <loganscott1225@gmail.com>
2026-08-05 09:29:09 -0500

    Reworked button functionality

--- src/gl_abstractions/include/Button.h
+++ src/gl_abstractions/include/Button.h
@@ -1,20 +1,36 @@
+// Button.h
 #pragma once
-#include <memory>
+#include "Texture.h"
 #include "Mesh.h"
-#include "GenQuad.h"
 #include "ShaderLink.h"
+#include <functional>
+#include "GenQuad.h"
+#include <memory>
 
 class Button {
 public:
+    Button(Cords cords, Texture texture, std::function<void()> onClick)
+        : cords(cords), texture(texture), onClick(std::move(onClick)) {
+        QuadMesh quad = generateTextureQuad(cords.xMin, cords.yMin, cords.xMax, cords.yMax);
+        mesh = std::make_unique<Mesh>(quad.vertices, quad.indices, /*hasUV=*/true);
+    }
 
-    Button(ShaderLink& shader, Cords cords, Color color);
+    void DrawButton(const ShaderLink& shader) const {
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture.id);
+        mesh->Draw();
+    }
 
-    bool Contains(float ndcX, float ndcY) const;
-    void Draw() const;
+    bool HitTest(float x, float y) const {
+        return x >= cords.xMin && x <= cords.xMax &&
+               y >= cords.yMin && y <= cords.yMax;
+    }
+
+    void Click() const { if (onClick) onClick(); }
 
 private:
-    ShaderLink& shader;
     Cords cords;
-    Color color;
+    Texture texture;
     std::unique_ptr<Mesh> mesh;
+    std::function<void()> onClick;
 };
